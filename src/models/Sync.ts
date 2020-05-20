9422a93c60698012f9e73c6879938e0bed35b676
Evgeniy <edubskiy@openwaygroup.com>
2020-05-21 00:33:22 +0300

    Use advanced generic types for Attributes class

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,14 +1,16 @@
-import { UserProps } from './User';
 import axios, { AxiosPromise } from "axios";
 
-export class Sync {
+interface HasId {
+  id?: number
+}
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
 
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data;
 
     if (id) {
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Sync } from './Sync';
 import { Eventing } from './Eventing';
 import axios, { AxiosResponse } from 'axios';
 
@@ -9,16 +10,9 @@ export interface UserProps {
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users'
+
 export class User {
   public events: Eventing = new Eventing()
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
 }
