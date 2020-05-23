b09e5e12eb32d99fc8ff1c370d8f9b03fed4cb56
Evgeniy <edubskiy@openwaygroup.com>
2020-05-23 23:20:36 +0300

    Wire up user attributes to user class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from './Attributes';
 import { Sync } from './Sync';
 import { Eventing } from './Eventing';
 import axios, { AxiosResponse } from 'axios';
@@ -15,4 +16,9 @@ const rootUrl = 'http://localhost:3000/users'
 export class User {
   public events: Eventing = new Eventing()
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
+  public attributes: Attributes<UserProps>
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs)
+  }
 }
