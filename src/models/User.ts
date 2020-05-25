8fbfad0121d08c4eb6c27664964ab120f0cf2496
Evgeniy <edubskiy@openwaygroup.com>
2020-05-25 15:09:07 +0300

    Passthrough user methods using get accessors

--- src/models/User.ts
+++ src/models/User.ts
@@ -21,4 +21,16 @@ export class User {
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs)
   }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
 }
