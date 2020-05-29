a9a2bf5063c512c604ca8626d03d5048b9dec4bf
Evgeniy <edubskiy@openwaygroup.com>
2020-05-29 18:48:37 +0300

    Finished Model class, User is now extended from Model with generic type of UserProps interface

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { Sync } from './Sync';
 import { Eventing } from './Eventing';
@@ -13,51 +14,4 @@ export interface UserProps {
 
 const rootUrl = 'http://localhost:3000/users'
 
-export class User {
-  public events: Eventing = new Eventing()
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
-  public attributes: Attributes<UserProps>
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs)
-  }
-
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update)
-    this.events.trigger('change')
-  }
-
-  fetch(): void {
-    const id = this.get('id')
-
-    if (typeof id !== 'number') {
-      throw new Error("Cannout  fetch without and id")
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data)
-    })
-  }
-
-  save(): void {
-    this.sync.save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger('save')
-      })
-      .catch(() => {
-        this.trigger('error')
-      })
-  }
-}
+export class User extends Model<UserProps> {}
