b528d6c57b15adacb0e3c0aa29eb53fc43b9e82f
Evgeniy <edubskiy@openwaygroup.com>
2020-06-03 01:12:17 +0300

    Added fetch method

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,9 +1,13 @@
+import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-import { User } from "./User";
+import { User, UserProps } from "./User";
 
 export class Collection {
   models: User[] = []
   events: Eventing = new Eventing()
+
+  constructor(public rootUrl: string) {
+  }
 
   get on() {
     return this.events.on
@@ -11,5 +15,16 @@ export class Collection {
 
   get trigger() {
     return this.events.trigger
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl).then((response: AxiosResponse) => {
+      response.data.forEach((value: UserProps) => {
+        const user = User.build(value)
+        this.models.push(user);
+      });
+
+      this.trigger('change')
+    })
   }
 }
