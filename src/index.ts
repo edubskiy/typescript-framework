ac9746478d3368b3943a8d90e4512553aeb626e3
Evgeniy <edubskiy@openwaygroup.com>
2020-06-06 01:44:45 +0300

    Add user model to UserForm class and render information from this model to our form

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'))
+const user = User.build({ name: 'name', age: 30})
+const userForm = new UserForm(document.getElementById('root'), user)
 
 userForm.render()
