f1c99d3dc2c8b62ac3ff81a0479215144b34d517
Evgeniy <edubskiy@openwaygroup.com>
2020-06-11 00:29:41 +0300

    Implemented view class regions

--- src/views/UserEdit.ts
+++ src/views/UserEdit.ts
@@ -2,6 +2,13 @@ import { User, UserProps } from './../models/User';
 import { View } from "./View";
 
 export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: '.user-show',
+      userFOrm: '.user-form'
+    }
+  }
+
   template() {
     return `
       <div>
