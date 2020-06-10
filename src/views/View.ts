b56a613f388188ee8062149ad783226f00a51da3
Evgeniy <edubskiy@openwaygroup.com>
2020-06-10 23:50:12 +0300

    Turned View class to generic class

--- src/views/View.ts
+++ src/views/View.ts
@@ -1,7 +1,6 @@
-import { User } from "../models/User";
-
-export abstract class View {
-  constructor(public parent: Element, public model: User) {
+import { Model } from './../models/Model';
+export abstract class View<T extends Model<K>, K> {
+  constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
 
